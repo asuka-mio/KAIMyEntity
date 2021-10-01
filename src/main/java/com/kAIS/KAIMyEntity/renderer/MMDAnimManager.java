9c3dae384dc08b1481128015237b2de3c48f38c6
tarsin <yuanqingxiang233@163.com>
2021-10-01 21:19:03 +0800

    Add default Anim when model anim unavailable

--- src/main/java/com/kAIS/KAIMyEntity/renderer/MMDAnimManager.java
+++ src/main/java/com/kAIS/KAIMyEntity/renderer/MMDAnimManager.java
@@ -1,6 +1,7 @@
 package com.kAIS.KAIMyEntity.renderer;
 
 import com.kAIS.KAIMyEntity.NativeFunc;
+import net.minecraft.client.Minecraft;
 
 import java.io.File;
 import java.util.Collection;
@@ -11,25 +12,12 @@ public class MMDAnimManager {
     static NativeFunc nf;
     static Map<String, Long> animStatic;
     static Map<IMMDModel, Map<String, Long>> animModel;
+    static String defaultAnimDir = new File(Minecraft.getInstance().gameDir, "KAIMyEntity/DefaultAnim").getAbsolutePath();
 
     public static void Init() {
         nf = NativeFunc.GetInst();
         animStatic = new HashMap<>();
         animModel = new HashMap<>();
-    }
-
-    //(Slash) For player (/Slash) Now player has multiple model. This function will be deleted.
-    public static long GetAnimStatic(IMMDModel model, String animName) {
-        String filename = GetAnimationFilename(model.GetModelDir(), animName);
-        Long result = animStatic.get(filename);
-        if (result == null) {
-            long anim = nf.LoadAnimation(model.GetModelLong(), filename);
-            if (anim == 0)
-                return 0;
-            result = anim;
-            animStatic.put(filename, result);
-        }
-        return result;
     }
 
     public static void AddModel(IMMDModel model) {
@@ -45,11 +33,14 @@ public class MMDAnimManager {
 
     //For other entity (Multiple model)
     public static long GetAnimModel(IMMDModel model, String animName) {
+        String defaultVmdFilename = GetAnimationFilename(defaultAnimDir, animName);
         String filename = GetAnimationFilename(model.GetModelDir(), animName);
         Map<String, Long> sub = animModel.get(model);
         Long result = sub.get(filename);
         if (result == null) {
             long anim = nf.LoadAnimation(model.GetModelLong(), filename);
+            if (anim == 0)
+                anim = nf.LoadAnimation(model.GetModelLong(), defaultVmdFilename);
             if (anim == 0)
                 return 0;
             result = anim;
